ba1702b6632b0efe622a90fe0f66afd139b22335
Lindell Carter <lindellcarternyc@gmail.com>
2018-04-23 17:14:52 -0400

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+// Icon styles
+import './assets/css/weather-icons-wind.css'
+import './assets/css/weather-icons.css'
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();
