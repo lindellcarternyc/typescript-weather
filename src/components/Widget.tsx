274889ec2c10b0e0fdcf9e9514ac372c095fe1d9
Lindell Carter <lindellcarternyc@gmail.com>
2018-04-24 22:48:07 -0400

    Add children to widget

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react'
 
-export default (props: { title: string }): JSX.Element => {
-  const { title } = props
+interface IWidgetProps {
+  title: string
+  children?: React.ReactNode
+}
+export default (props: IWidgetProps): JSX.Element => {
+  const { title, children } = props
   return (
     <div
       style={{
         border: '1px solid white',
+        overflow: 'hidden',
         padding: '0.5rem'
       }}
     >
@@ -13,9 +18,11 @@ export default (props: { title: string }): JSX.Element => {
         style={{
           borderBottom: '1px solid white',
           fontWeight: 100,
+          marginBottom: '0.25rem',
           paddingBottom: '0.25rem'
         }}
       >{title}</h4>
+      {children}
     </div>
   )
 }
