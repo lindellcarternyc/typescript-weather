459d2d85fc9b9fbd84b11f56ea5754ab83686afb
Lindell Carter <lindellcarternyc@gmail.com>
2018-04-26 15:33:02 -0400

    added padding to box

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -8,14 +8,16 @@ interface IBoxProps {
     justifyContent?: string
   }
   bordered?: boolean
+  padded?: boolean
 }
 export default (props: IBoxProps): JSX.Element => {
-  const { bordered, children, styles } = props
+  const { bordered, children, padded, styles } = props
   return (
     <div style={{
       border: bordered ? '1px solid white' : undefined,
       margin: '1rem auto 0.5rem',
       maxWidth: '600px',
+      padding: padded ? '0.5rem' : 0,
       position: 'relative',
       ...styles
     }}>
